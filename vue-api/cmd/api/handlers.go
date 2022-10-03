77d3e5c8d81cd4271fc0c0746fc29aaeb9318c90
Vlad <vladfoton86@gmail.com>
2022-10-03 12:02:36 +0300

    Check mistake with SQL
    Check mistake with SQL

--- vue-api/cmd/api/handlers.go
+++ vue-api/cmd/api/handlers.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"time"
 )
 
 type jsonResponse struct {
 	Error   bool        `json:"error"`
 	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
+	Data    interface{} `json:"data,omitempty"`
 }
+
+type envelope map[string]interface{}
 
 func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 	type credentials struct {
@@ -30,9 +34,37 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 	//TODO authenticate
 	app.infoLog.Println(creds.UserName, creds.Password)
 
+	// look up the user by email
+	user, err := app.models.User.GetByEmail(creds.UserName)
+	if err != nil {
+		app.errorJSON(w, errors.New("invalid username/password"))
+		return
+	}
+	// validate the user's password
+	validPassword, err := user.PasswordMatches(creds.Password)
+	if err != nil || !validPassword {
+		app.errorJSON(w, errors.New("invalid username/password"))
+		return
+	}
+	//we have a valid user, so generate a token
+	token, err := app.models.Token.GenerateToken(user.ID, 24*time.Hour)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	//save it to the database
+	err = app.models.Token.Insert(*token, *user)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 	// send back a response
-	payload.Error = false
-	payload.Message = "Signed in"
+	payload = jsonResponse{
+		Error:   false,
+		Message: "logged in",
+		Data:    envelope{"token": token},
+	}
 
 	err = app.writeJSON(w, http.StatusOK, payload)
 	if err != nil {

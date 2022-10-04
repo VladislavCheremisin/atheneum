3202522280fae14561ee1962c4de61598a4960cc
Vlad <vladfoton86@gmail.com>
2022-10-04 23:31:57 +0300

    Created .env file (2)
    Created .env file (2)

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -65,7 +65,7 @@ name:'App',
       headers: headers,
     }
 
-    fetch("http://localhost:8081/admin/foo", requestOptions)
+    fetch(process.env.VUE_APP_API_URL + "/admin/foo", requestOptions)
         .then((response) => response.json())
         .then((data) => {
           console.log(data)

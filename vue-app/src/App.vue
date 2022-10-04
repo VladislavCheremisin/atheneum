98e80aa2da22e2b25a77cce155d70993887821de
Vlad <vladfoton86@gmail.com>
2022-10-04 09:44:14 +0300

    Cookie
    Cookie

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -13,6 +13,13 @@ import HeaderGlobal from "./components/HeaderGlobal"
 import FooterGlobal from "./components/FooterGlobal"
 import { store } from './components/store.js'
 
+const getCookie = (name) => {
+  return document.cookie.split("; ").reduce((r, v) => {
+    const parts = v.split("=");
+    return  parts[0] === name ? decodeURIComponent(parts[1]) : r;
+  }, "");
+}
+
 
 
 export default {
@@ -24,6 +31,23 @@ name:'App',
   data() {
     return {
       store
+    }
+  },
+  beforeMount() {
+  //check for a cookie
+    let data = getCookie("_site_data");
+
+    if (data !== "") {
+      let cookieData = JSON.parse(data);
+
+    //  update store
+      store.token = cookieData.token.token;
+      store.user = {
+        id: cookieData.user.id,
+        first_name: cookieData.user.first_name,
+        last_name: cookieData.user.last_name,
+        email: cookieData.user.email,
+      }
     }
   }
 }

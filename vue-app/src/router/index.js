aebc58d60920faa7ccdbbe45b27acca3f5a8424e
Vlad <vladfoton86@gmail.com>
2022-10-07 10:39:48 +0300

    Inserting data
    Adding database tables for Books, Authors, and Genres and trying out our ValidateToken handler and Security.checkToken() functionality.

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -7,6 +7,7 @@ import BooksAdmin from './../components/BooksAdmin.vue'
 import BookEdit from './../components/BookEdit.vue'
 import Users from './../components/Users.vue'
 import User from './../components/UserEdit.vue'
+import Security from "@/components/security";
 
 
 
@@ -58,4 +59,7 @@ const routes = [
     },
 ]
 const router = createRouter({history: createWebHistory(), routes})
+router.beforeEach(() => {
+    Security.checkToken();
+})
 export default router

47bc24191a02ac1a01dc83c21545ff71e001bec3
Vlad <vladfoton86@gmail.com>
2022-10-14 14:46:40 +0300

    Testing write JSON read JSON
    Testing write JSON read JSON

---
+++ vue-api/cmd/api/helpers_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_readJSON(t *testing.T) {
+	//	simple json
+	sampleJSON := map[string]interface{}{
+		"foo": "bar",
+	}
+
+	body, _ := json.Marshal(sampleJSON)
+
+	var decodedJSON struct {
+		FOO string `json:"foo"`
+	}
+	//request
+	req, err := http.NewRequest("POST", "/", bytes.NewReader(body))
+	if err != nil {
+		t.Log(err)
+	}
+
+	// response recorder
+	rr := httptest.NewRecorder()
+	defer req.Body.Close()
+
+	// call readJSON
+	err = testApp.readJSON(rr, req, &decodedJSON)
+	if err != nil {
+		t.Error("failed to decode json", err)
+	}
+}
+
+func Test_writeJSON(t *testing.T) {
+	rr := httptest.NewRecorder()
+	payload := jsonResponse{
+		Error:   false,
+		Message: "foo",
+	}
+
+	headers := make(http.Header)
+	headers.Add("FOO", "BAR")
+	err := testApp.writeJSON(rr, http.StatusOK, payload, headers)
+	if err != nil {
+		t.Errorf("failed to write JSON: %v", err)
+	}
+
+	testApp.enviroment = "production"
+	err = testApp.writeJSON(rr, http.StatusOK, payload, headers)
+	if err != nil {
+		t.Errorf("failed to write JSON in production: %v", err)
+	}
+
+	testApp.enviroment = "development"
+}
